7f97576e53521c74ea323dd1375fffb7b6d5abd4
nde booy <ndesimoniche@gmail.com>
2024-01-29 07:25:41 +0400

    adjusted my vs code

--- src/components/App.js
+++ src/components/App.js
@@ -6,41 +6,35 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Questions";
 
-
-
-
-
-
 const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
   index: 0,
   answer: null,
-  points:0
-}
-
+  points: 0,
+};
 
 function reducer(state, action) {
   switch (action.type) {
-
     case "dataReceived":
       return {
         ...state,
         questions: action.payload,
-        status: "ready"
-      }
+        status: "ready",
+      };
 
     case "dataFailed":
       return {
         ...state,
-        status: "error"
-      }
+        status: "error",
+      };
 
     case "start":
       return {
-        ...state, status: "active"
-      }
+        ...state,
+        status: "active",
+      };
 
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -48,29 +42,32 @@ function reducer(state, action) {
       return {
         ...state,
         answer: action.payload,
-        
-        points: action.payload === question.correctOption
-          ? state.points + question.points : state.points
 
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
 
-      }
-
-    default: throw new Error('unknown Error')
+    default:
+      throw new Error("unknown Error");
   }
-
 }
 
-
 function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState)
-
-  const numQuestions = questions.length
-
+  const numQuestions = questions.length;
 
   useEffect(() => {
-    fetch("http://localhost:9000/questions").then((res) => res.json()).then((data) => dispatch({ type: "dataReceived", payload: data })).catch((error) => dispatch({ type: "dataFailed" }))
-  }, [])
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
     <div className="app">
@@ -78,8 +75,16 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question questions={questions[index]} dispatch={dispatch} answer={answer} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            questions={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,19 +1,31 @@
-
-
 const Options = ({ questions, dispatch, answer }) => {
-    const hasAnswer = answer !== null
+  const hasAnswer = answer !== null;
 
-    return (
-        <div className="options">
-            {questions.options.map((option, index) => <button className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswer ? index === questions.correctOption ? "correct" : "wrong" : ""}`}
-                key={option}
-                disabled={hasAnswer}
-                onClick={() => dispatch({
-                    type: "newAnswer",
-                    payload: index
-                })}>{option}</button>)}
-        </div>
-    )
-}
+  return (
+    <div className="options">
+      {questions.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer
+              ? index === questions.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswer}
+          onClick={() =>
+            dispatch({
+              type: "newAnswer",
+              payload: index,
+            })
+          }
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default Options
+export default Options;
